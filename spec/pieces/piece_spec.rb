5a12646acc485482c8157558dc2a4124e09a7f36
Lucas Schraier <lucasfschraier@gmail.com>
2019-02-17 12:19:34 -0500

    Describe tests for Piece class

--- spec/pieces/piece_spec.rb
+++ spec/pieces/piece_spec.rb
@@ -1,1 +1,37 @@
 require "piece.rb"
+
+    describe Piece do
+
+        describe "#initialize" do
+
+            it "should set the piece's color, board, and position"
+
+        end
+
+        describe "#to_s" do
+
+            it "should return a string representation of the piece"
+
+        end
+
+        describe "#empty" do 
+
+            it "should return true if the piece is the NullPiece"
+
+            it "should return false otherwise"
+
+        end
+
+        describe "#valid_moves" do
+
+            it "should return an array of all positions on the board the piece can legally move to"
+
+        end
+
+        describe "#symbol" do
+
+            it "should return a symbol representation of the piece"
+
+        end
+
+    end
