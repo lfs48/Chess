0ec11b7dfeabedbb76b688cf4a57fd182797cc3c
Lucas Schraier <lucasfschraier@gmail.com>
2019-02-17 13:41:04 -0500

    Describe tests for slideable module

---
+++ spec/pieces/slideable_spec.rb
@@ -0,0 +1,19 @@
+require "pieces/slideable.rb"
+
+describe Slideable do
+
+    describe "#moves" do
+
+        it "should return an array of positions"
+
+        it "should only return positions in directions that piece can move to"
+
+        it "should stop at positions occupied by friendly pieces"
+
+        it "should allow movement into the position of the first enemy piece in a given direction"
+
+        it "should not include positions past the first enemy piece"
+
+    end
+
+end
