d271a993132b50f979bed3a6426f852480b54692
Lucas Schraier <lucasfschraier@gmail.com>
2019-02-17 13:47:24 -0500

    Write tests for stepable module

---
+++ spec/pieces/stepable_spec.rb
@@ -0,0 +1,21 @@
+require "pieces/stepable.rb"
+
+describe Stepable do
+
+    describe "#moves" do
+
+        it "should return an array of positions"
+
+        it "should not include positions occupied by friendly pieces"
+
+        it "should only include positions within the bounds of the board"
+
+        it "should work for a Pawn"
+
+        it "should work for a Knight"
+
+        it "should work for a King"
+
+    end
+
+end
