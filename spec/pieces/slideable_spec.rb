5948b6c17ae4671b6f86df29a94b42958b1eeb36
Lucas Schraier <lucasfschraier@gmail.com>
2019-02-17 13:44:40 -0500

    Edit tests for slideable module to test rook, bishop, and queen movement separately

--- spec/pieces/slideable_spec.rb
+++ spec/pieces/slideable_spec.rb
@@ -6,13 +6,17 @@ describe Slideable do
 
         it "should return an array of positions"
 
-        it "should only return positions in directions that piece can move to"
-
         it "should stop at positions occupied by friendly pieces"
 
         it "should allow movement into the position of the first enemy piece in a given direction"
 
         it "should not include positions past the first enemy piece"
+
+        it "should work for a Rook"
+
+        it "should work for a Bishop"
+
+        it "should work for a Queen"
 
     end
 
